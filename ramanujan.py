b04696e7bf0888e2836ac6c62efc30a0d0ccf06d
James Church <jcchurch@gmail.com>
2020-02-17 14:51:09 -0600

    Formats Ramanujan Code to be easier to read

--- ramanujan.py
+++ ramanujan.py
@@ -1,6 +1,9 @@
 import math
 
-def nCr(n,r): return math.factorial(n) / math.factorial(r) / math.factorial(n-r)
-def PiRamanujan(n): return 1/sum([nCr(2*i,i)**3 * (42*i+5) / 2**(12*i+4) for i in range(n)])
+def nCr(n,r):
+    return math.factorial(n) / math.factorial(r) / math.factorial(n-r)
+
+def PiRamanujan(n):
+    return 1/sum([nCr(2*i,i)**3 * (42*i+5) / 2**(12*i+4) for i in range(n)])
 
 print(PiRamanujan(10))
